d7e59a81db1abf404d67860139a6500ca5f5cabc
jmjusmin <jmjusmin1412@gmail.com>
2022-10-23 22:56:20 -0700

    Complete dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,7 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function Dropdown({ options, selected, onSelectedChange }) {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const onBodyClick = (e) => {
+      if (ref.current.contains(e.target)) {
+        return;
+      }
+      // console.log(e.target);
+      setOpen(false);
+    };
+
+    document.body.addEventListener("click", onBodyClick);
+
+    //clean up function
+    return () => {
+      document.body.removeEventListener("click", onBodyClick);
+    };
+  }, []); //run only one time
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
@@ -11,7 +29,9 @@ function Dropdown({ options, selected, onSelectedChange }) {
         <div
           key={option.value}
           className="item"
-          onClick={() => onSelectedChange(option)}
+          onClick={() => {
+            onSelectedChange(option);
+          }}
         >
           {option.label}
         </div>
@@ -20,11 +40,13 @@ function Dropdown({ options, selected, onSelectedChange }) {
   });
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
         <div
-          onClick={() => setOpen(!open)}
+          onClick={() => {
+            setOpen(!open);
+          }}
           className={`ui selection dropdown ${open ? "visible active" : ""}`}
         >
           <i className="dropdown icon"></i>
