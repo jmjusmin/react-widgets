ac27267f56d6ee5838148a52044be8c00ebc9aed
jmjusmin <jmjusmin1412@gmail.com>
2022-10-25 14:59:15 -0700

    Implement the Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React, { useState } from "react";
-// import Accordion from "./components/Accordion";
-// import ItemsData from ".data/ItemsData";
-// import Search from "./components/Search";
-// import Dropdown from "./components/Dropdown";
-// import ColorData from "./data/ColorData";
+import Header from "./components/Header";
+import Accordion from "./components/Accordion";
+import ItemsData from "./data/ItemsData";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import ColorData from "./data/ColorData";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
 function App() {
-  // const [selected, setSelected] = useState(ColorData[0]);
+  const [selected, setSelected] = useState(ColorData[0]);
   return (
     <div>
-      {/* <Accordion items={ItemsData} /> */}
-      {/* <Search /> */}
-      {/* <Dropdown
-        options={ColorData}
-        selected={selected}
-        onSelectedChange={setSelected}
-      /> */}
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={ItemsData} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a Langugage"
+          options={ColorData}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 }
